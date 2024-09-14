d58ab2b72880b2763168b05cdbd8ea09a1483b64
vik-an <vandruskevic88@gmail.com>
2024-09-14 19:49:12 +0300

    added files 'generalInfo' and styling in header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import { HeaderComp } from "./components/headerSet.jsx";
-
+import { MainPage } from "./components/generalInfo.jsx";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <HeaderComp />
       </header>
+
+      <div>
+        <MainPage />
+        <h1>puritonas</h1>
+      </div>
     </div>
   );
 }
---
+++ src/components/generalInfo.jsx
@@ -0,0 +1,15 @@
+import "./generalInfo.css";
+
+export const MainPage = () => {
+  return (
+    <div className="">
+      <h1>Find Home Service/Repair</h1>
+      <h1>Near You</h1>
+      <p>Explore Best Home Service & Repair near you</p>
+
+      <input placeholder=" Search" />
+
+      <button onClick={() => alert("Not today")}>Login/Sing UP</button>
+    </div>
+  );
+};
